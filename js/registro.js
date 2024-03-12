283833caab5e3298811ff4706e10a01c2fe8bd17
rafael07-dev <deralato817@gmail.com>
2024-03-12 11:52:40 -0500

    funcionalidad para solo poder marcar un solo dia de talleres

--- js/registro.js
+++ js/registro.js
@@ -10,39 +10,63 @@
     var btnCalcular = document.getElementById('btnCalcular');
     var talleres = document.querySelector('.t-seleccionados');
 
-    //cards de precios
+    //input cards de precios
     var paseDia = document.getElementById('paseDia');
     var paseDosDias = document.getElementById('paseDosDias');
     var paseTodos = document.getElementById('paseTodos');
 
+    //checkboxes
+    const checkboxesViernes = document.querySelectorAll('.viernes .campo-taller input[type="checkbox"]');
+    const checkboxesSabado = document.querySelectorAll('.sabado .campo-taller input[type="checkbox"]');
+    const checkboxesDomingo = document.querySelectorAll('.domingo .campo-taller input[type="checkbox"]');
+    //const viernes = document.getElementById('viernes');
+
     document.addEventListener("DOMContentLoaded", function () {
 
+        //agregar evento click a todos los checkbox
+        checkboxesViernes.forEach(item => item.addEventListener('click', updateInputs));
+        checkboxesSabado.forEach(item => item.addEventListener('click', updateInputs));
+        checkboxesDomingo.forEach(item => item.addEventListener('click', updateInputs));
+
+        //boton general para calcular precio
         btnCalcular.addEventListener('click', validateInputChecked);
-        getValueInputs(paseDia);
-        getValueInputs(paseDosDias);
-        getValueInputs(paseTodos);
+
+        //obtener valores de los inputs de las cards talleres
+        handleInput(paseDia);
+        handleInput(paseDosDias);
+        handleInput(paseTodos);
     });
-    
-    function getValueInputs(element) {
+
+    function handleInput(element) {
 
         element.addEventListener('input', () => {
+            //mostrar todos los talleres
+            showTalleres(wapperTallerTodos)
+            checkedInputs(element)
+        });
+    }
 
-            wapperTallerTodos.forEach(item => {
-               if (!item.classList.contains('show')) {
-                    item.classList.toggle('show');
-                }
-            });
+    function checkedInputs(element) {
+        if (element.id === 'paseDia') {
+            paseDosDias.setAttribute('disabled', 'true');
+            paseTodos.setAttribute('disabled', 'true');
+        }
+    }
+
+    function showTalleres(elements) {
+        elements.forEach(item => {
+            if (!item.classList.contains('show')) {
+                item.classList.toggle('show');
+            }
         });
     }
 
     function validateInputChecked() {
-        const checkboxesViernes = document.querySelectorAll('.viernes .campo-taller input[type="checkbox"]');
-        //const checboxesSabado = document.querySelectorAll('.sabado .campo-taller input[type="checkbox"]');
-        //const checboxesDomingo = document.querySelectorAll('.domingo .campo-taller input[type="checkbox"]');
 
         checkboxesViernes.forEach(item => {
+
             if (item.checked) {
-                const label = document.querySelector('label[for="'+ item.id +'"]');
+                const label = document.querySelector('label[for="' + item.id + '"]');
                 const li = document.createElement('li');
 
                 li.innerText = label.textContent;
@@ -52,7 +76,6 @@
         });
 
         calculatePrice();
-
     }
 
     function calculatePrice() {
@@ -70,12 +93,29 @@
             totalPrice += paseTodos.value * VALOR_PASE_TODOS;
         }
 
-        total.innerText = "$"+ totalPrice;
-
+        total.innerText = "$" + totalPrice;
     }
 
-    function update() {
-        
+    function updateInputs(e) {
+        if (e.target.id <= 15) {
+            disableCheckBox(checkboxesSabado);
+            disableCheckBox(checkboxesDomingo);
+        }
+
+        if (e.target.id > 15 && e.target.id <= 30) {
+            disableCheckBox(checkboxesViernes);
+            disableCheckBox(checkboxesDomingo);
+        }
+
+        if (e.target.id > 30) {
+            disableCheckBox(checkboxesViernes);
+            disableCheckBox(checkboxesSabado);
+        }
     }
 
+    function disableCheckBox(element) {
+        element.forEach(item => {
+            item.setAttribute('disabled', 'true');
+        });
+    }
 })();
