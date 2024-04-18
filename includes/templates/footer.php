134e31961b20dd6edb877159f807999777ceed7e
rafael07-dev <deralato817@gmail.com>
2024-04-18 11:00:43 -0500

    cambiando ubicacion de templates

---
+++ includes/templates/footer.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>CTGWEBCAMP</title>
+    <link rel="stylesheet" href="css/all.min.css">
+    <link rel="stylesheet" href="css/style.css">
+</head>
+
+<body>
+    <footer>
+        <div class="wrapper-footer contenedor">
+            <div class="acerca-de">
+                <h1>Sobre <span>ctg.webcamp</span></h1>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut dignissimos suscipit recusandae. Tenetur
+                    sunt ipsam modi omnis quisquam sapiente, harum voluptatum eveniet reprehenderit sed soluta aperiam
+                    perferendis quis illo praesentium!</p>
+            </div>
+
+            <div class="tweets">
+                <h1>Ultimos <span>tweets</span></h1>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut dignissimos suscipit recusandae. Tenetur
+                    sunt ipsam modi omnis quisquam sapiente, harum voluptatum eveniet reprehenderit sed soluta aperiam
+                    perferendis quis illo praesentium!</p>
+                <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Labore reiciendis mollitia, in sequi est
+                    sit culpa quasi consequuntur qui dolorem modi assumenda, earum quam quisquam rem quo. Dolorum, nihil
+                    fugit!</p>
+            </div>
+
+            <div class="redes-sociales">
+                <h1>Redes <span>sociales</span></h1>
+                <ul>
+                    <li><a href="#"><i class="fa-brands fa-facebook"></i></a></li>
+                    <li><a href="#"><i class="fa-brands fa-instagram"></i></a></li>
+                    <li><a href="#"><i class="fa-brands fa-youtube"></i></a></li>
+                    <li><a href="#"><i class="fa-brands fa-twitter"></i></a></li>
+                    <li><a href="#"><i class="fa-brands fa-pinterest"></i></a></li>
+                </ul>
+            </div>
+        </div>
+    </footer>
+
+    <script src="js/script.js"></script>
+</body>
+
+</html>
