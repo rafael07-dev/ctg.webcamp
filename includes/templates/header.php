37a030e9dc9f16a473f3a314732919ac2d1e6cd1
rafael07-dev <deralato817@gmail.com>
2024-04-26 08:35:32 -0500

    refactorizando codigo

--- includes/templates/header.php
+++ includes/templates/header.php
@@ -1,74 +1,26 @@
-<!DOCTYPE html>
-<html lang="en">
 
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>CTGWEBCAMP</title>
-</head>
-
-<body>
-    <header class="site-header">
-        <div class="hero">
-            <div class="contenido-hero">
-                <nav class="redes-sociales contenedor-redes">
-                    <ul>
-                        <li><a href="#"><i class="fa-brands fa-facebook"></i></a></li>
-                        <li><a href="#"><i class="fa-brands fa-instagram"></i></a></li>
-                        <li><a href="#"><i class="fa-brands fa-youtube"></i></a></li>
-                        <li><a href="#"><i class="fa-brands fa-twitter"></i></a></li>
-                        <li><a href="#"><i class="fa-brands fa-pinterest"></i></a></li>
-                    </ul>
-                </nav>
-
-                <div class="informacion-evento">
-
-                    <div class="iconos">
-                        <p class="fecha"><i class="fa-solid fa-calendar-days"></i>01-2-2024</p>
-                        <p class="ciudad"><i class="fa-solid fa-location-dot"></i></i>Cartagena, Colombia</p>
-                    </div>
-
-                    <h1>Ctg.WebCamp</h1>
-                    <p class="slogan">la mejor conferencia de <span>desarrollo web</span></p>
-                </div>
-            </div>
-        </div>
-    </header>
-
-    <div class="barra">
-        <div class="contenedor-barra">
-            <div class="logo">
-                <img src="img/logo.svg" alt="logo ctgwebcamp">
-            </div>
-            <nav class="barra-navegacion">
+<header class="site-header">
+    <div class="hero">
+        <div class="contenido-hero">
+            <nav class="redes-sociales contenedor-redes">
                 <ul>
-                    <li><a href="#">conferencia</a></li>
-                    <li><a href="calendario.php">calendario</a></li>
-                    <li><a href="#">invitados</a></li>
-                    <li><a href="registro.php">reservaciones</a></li>
+                    <li><a href="#"><i class="fa-brands fa-facebook"></i></a></li>
+                    <li><a href="#"><i class="fa-brands fa-instagram"></i></a></li>
+                    <li><a href="#"><i class="fa-brands fa-youtube"></i></a></li>
+                    <li><a href="#"><i class="fa-brands fa-twitter"></i></a></li>
+                    <li><a href="#"><i class="fa-brands fa-pinterest"></i></a></li>
                 </ul>
             </nav>
 
-            <div class="menu-bar">
-                <a href="#"><svg xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 -960 960 960" width="24">
-                        <path d="M120-240v-80h720v80H120Zm0-200v-80h720v80H120Zm0-200v-80h720v80H120Z" />
-                    </svg></a>
+            <div class="informacion-evento">
+
+                <div class="iconos">
+                    <p class="fecha"><i class="fa-solid fa-calendar-days"></i>01-2-2024</p>                        <p class="ciudad"><i class="fa-solid fa-location-dot"></i></i>Cartagena, Colombia</p>
+                </div>
+
+                <h1>Ctg.WebCamp</h1>
+                <p class="slogan">la mejor conferencia de <span>desarrollo web</span></p>
             </div>
         </div>
     </div>
-
-    <div class="dropdown-menu">
-        <ul>
-            <li><a class="btn-close" href="#"><svg xmlns="http://www.w3.org/2000/svg" height="24"
-                        viewBox="0 -960 960 960" width="24">
-                        <path
-                            d="m256-200-56-56 224-224-224-224 56-56 224 224 224-224 56 56-224 224 224 224-56 56-224-224-224 224Z" />
-                    </svg></a></li>
-            <li><a href="#">conferencia</a></li>
-            <li><a href="calendario.php">calendario</a></li>
-            <li><a href="invitados.php">invitados</a></li>
-            <li><a href="registro.php">reservaciones</a></li>
-        </ul>
-    </div>
-
-</body>
+</header>
