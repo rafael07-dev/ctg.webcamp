230179aca61b6dd65f0dc2af4aff7e92edab2e1b
rafael07-dev <deralato817@gmail.com>
2024-04-26 08:29:46 -0500

    agregando nuevo template para refactorizar codigo

---
+++ includes/templates/barra.php
@@ -0,0 +1,31 @@
+<div class="barra">
+    <div class="contenedor-barra">
+        <div class="logo">
+            <img src="img/logo.svg" alt="logo ctgwebcamp">
+        </div>
+        <nav class="barra-navegacion">
+            <ul>
+                <li><a href="#">conferencia</a></li>
+                <li><a href="calendario.php">calendario</a></li>
+                <li><a href="invitados.php">invitados</a></li>
+                <li><a href="registro.php">reservaciones</a></li>
+            </ul>
+        </nav>
+
+        <div class="menu-bar">
+            <a href="#"><svg xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 -960 960 960" width="24">
+                <path d="M120-240v-80h720v80H120Zm0-200v-80h720v80H120Zm0-200v-80h720v80H120Z" />
+                </svg></a>
+        </div>
+    </div>
+</div>
+
+<div class="dropdown-menu">
+    <ul>
+        <li><a class="btn-close" href="#"><svg xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 -960 960 960" width="24"><path d="m256-200-56-56 224-224-224-224 56-56 224 224 224-224 56 56-224 224 224 224-56 56-224-224-224 224Z" /></svg></a></li>
+        <li><a href="#">conferencia</a></li>
+        <li><a href="calendario.php">calendario</a></li>
+        <li><a href="invitados.php">invitados</a></li>
+        <li><a href="registro.php">reservaciones</a></li>
+    </ul>
+</div>
