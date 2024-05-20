8f9d1bd0f242799d7c73fba875e87c61090f22ac
rafael07-dev <deralato817@gmail.com>
2024-05-20 10:04:47 -0500

    Ruta principal

---
+++ app/core/app.php
@@ -0,0 +1,32 @@
+<?php 
+    class App{
+
+        public function __construct()
+        {
+            
+        }
+
+        public function run()
+        {
+            $url = $_GET['uri'];
+            $url = rtrim($url, '/');
+            $url = explode('/', $url);
+
+            $controllerName = $url[0] . "Controller";
+            $pathController = "./app/controllers/".$controllerName.".php";
+
+
+            if (file_exists($pathController)) {
+                require_once($pathController);
+
+                $controller = new $controllerName;
+
+                $methodName = isset($url[1]) ? $url[1] : "index";
+
+                $controller->{$methodName}();
+            }
+
+        }
+    }
+
+?>
