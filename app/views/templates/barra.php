cb27003d2a6251743d83628b301df93edbccafae
rafael07-dev <deralato817@gmail.com>
2024-05-21 10:49:20 -0500

    Refactor

--- app/views/templates/barra.php
+++ app/views/templates/barra.php
@@ -12,7 +12,6 @@
                 <li><a href="/ctg.webcamp/reservaciones">reservaciones</a></li>
             </ul>
         </nav>
-
         <div class="menu-bar">
             <a href="#"><svg xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 -960 960 960" width="24">
                 <path d="M120-240v-80h720v80H120Zm0-200v-80h720v80H120Zm0-200v-80h720v80H120Z" />
