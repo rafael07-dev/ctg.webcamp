1ca98b2f23488f04d7b335743ea41a1438e42b46
rafael07-dev <deralato817@gmail.com>
2024-05-21 09:15:14 -0500

    incluyendo archivos de configuración y conexion a base de datos, al inicio de la aplicacion para uso general

--- index.php
+++ index.php
@@ -1,6 +1,7 @@
 <?php
-
-    require_once(__DIR__."/app/core/app.php");
+    require(__DIR__."/config/config.php");
+    require(__DIR__."/config/db_conexion.php");
+    require(__DIR__."/app/core/app.php");
 
     $app = new App();
     $app->run();
